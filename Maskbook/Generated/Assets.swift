9be3834a889fa86332fe5db64f2982258b88175e
Hugo <cythbgy@gmail.com>
2022-03-02 10:36:41 +0800

    Fix/balance UI (#31)
    * restyling card view
    
    * udate shadow of account card
    
    * fix: use `blockchain` instead of `blockchain.rawValue`
    
    * remove a wrong case
    
    * fix: use `.polka` instead of `.polkd`
    
    Co-authored-by: Hugo L <hugol@MacBook-Pro-2.local>

--- Maskbook/Generated/Assets.swift
+++ Maskbook/Generated/Assets.swift
@@ -23,31 +23,35 @@ internal typealias AssetImageTypeAlias = ImageAsset.Image
 internal enum Asset {
   internal enum Colors {
     internal enum AccountCard {
-      internal static let background1 = ColorAsset(name: "Colors/AccountCard/Background1")
-      internal static let background2 = ColorAsset(name: "Colors/AccountCard/Background2")
       internal static let addressText = ColorAsset(name: "Colors/AccountCard/address_text")
       internal static let buttonBg = ColorAsset(name: "Colors/AccountCard/button_bg")
       internal enum Chains {
         internal static let all1 = ColorAsset(name: "Colors/AccountCard/chains/all1")
         internal static let all2 = ColorAsset(name: "Colors/AccountCard/chains/all2")
+        internal static let all3 = ColorAsset(name: "Colors/AccountCard/chains/all3")
         internal static let arbitrum1 = ColorAsset(name: "Colors/AccountCard/chains/arbitrum1")
         internal static let arbitrum2 = ColorAsset(name: "Colors/AccountCard/chains/arbitrum2")
-        internal static let bottomEnd = ColorAsset(name: "Colors/AccountCard/chains/bottomEnd")
-        internal static let bottomStart = ColorAsset(name: "Colors/AccountCard/chains/bottomStart")
+        internal static let arbitrum3 = ColorAsset(name: "Colors/AccountCard/chains/arbitrum3")
         internal static let bsc1 = ColorAsset(name: "Colors/AccountCard/chains/bsc1")
         internal static let bsc2 = ColorAsset(name: "Colors/AccountCard/chains/bsc2")
+        internal static let bsc3 = ColorAsset(name: "Colors/AccountCard/chains/bsc3")
         internal static let eth1 = ColorAsset(name: "Colors/AccountCard/chains/eth1")
         internal static let eth2 = ColorAsset(name: "Colors/AccountCard/chains/eth2")
+        internal static let eth3 = ColorAsset(name: "Colors/AccountCard/chains/eth3")
         internal static let matic1 = ColorAsset(name: "Colors/AccountCard/chains/matic1")
         internal static let matic2 = ColorAsset(name: "Colors/AccountCard/chains/matic2")
+        internal static let matic3 = ColorAsset(name: "Colors/AccountCard/chains/matic3")
         internal static let optimism1 = ColorAsset(name: "Colors/AccountCard/chains/optimism1")
         internal static let optimism2 = ColorAsset(name: "Colors/AccountCard/chains/optimism2")
+        internal static let optimism3 = ColorAsset(name: "Colors/AccountCard/chains/optimism3")
         internal static let xdai1 = ColorAsset(name: "Colors/AccountCard/chains/xdai1")
         internal static let xdai2 = ColorAsset(name: "Colors/AccountCard/chains/xdai2")
+        internal static let xdai3 = ColorAsset(name: "Colors/AccountCard/chains/xdai3")
       }
       internal static let nameText = ColorAsset(name: "Colors/AccountCard/name_text")
       internal static let wcBackground1 = ColorAsset(name: "Colors/AccountCard/wcBackground1")
       internal static let wcBackground2 = ColorAsset(name: "Colors/AccountCard/wcBackground2")
+      internal static let wcBackground3 = ColorAsset(name: "Colors/AccountCard/wcBackground3")
     }
     internal enum Background {
       internal static let bar = ColorAsset(name: "Colors/Background/bar")
@@ -148,6 +152,15 @@ internal enum Asset {
     internal static let segmentShadow = ColorAsset(name: "Colors/segmentShadow")
     internal enum Shadow {
       internal static let accoundCard = ColorAsset(name: "Colors/shadow/accoundCard")
+      internal enum Card {
+        internal static let all = ColorAsset(name: "Colors/shadow/card/all")
+        internal static let arbitrum = ColorAsset(name: "Colors/shadow/card/arbitrum")
+        internal static let bsc = ColorAsset(name: "Colors/shadow/card/bsc")
+        internal static let eth = ColorAsset(name: "Colors/shadow/card/eth")
+        internal static let matic = ColorAsset(name: "Colors/shadow/card/matic")
+        internal static let wallet = ColorAsset(name: "Colors/shadow/card/wallet")
+        internal static let xdai = ColorAsset(name: "Colors/shadow/card/xdai")
+      }
       internal static let primaryButton = ColorAsset(name: "Colors/shadow/primaryButton")
       internal static let sendButton = ColorAsset(name: "Colors/shadow/sendButton")
       internal static let sesondaryButton = ColorAsset(name: "Colors/shadow/sesondaryButton")
--- Maskbook/Scene/Wallet/Balance/View/AccountCardView.swift
+++ Maskbook/Scene/Wallet/Balance/View/AccountCardView.swift
@@ -5,6 +5,7 @@
 //  Created by BradGao on 2021/6/4.
 //  Copyright © 2021 dimension. All rights reserved.
 //
+// swiftlint:disable force_cast line_length type_body_length file_length
 
 import Combine
 import CoreDataStack
@@ -38,14 +39,8 @@ class AccountCardView: UIView {
         return layer1
     }()
     
-    private var stackViewGradientLayer: CAGradientLayer = {
-        let layer1 = CAGradientLayer()
-        layer1.colors = [
-            Asset.Colors.AccountCard.Chains.bottomStart.color.cgColor,
-            Asset.Colors.AccountCard.Chains.bottomEnd.color.cgColor
-        ]
-        layer1.startPoint = CGPoint(x: 0.5, y: 0)
-        layer1.endPoint = CGPoint(x: 1, y: 1)
+    private lazy var stackViewBackgroudLayer: CALayer = {
+        let layer1 = CALayer()
         layer1.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         layer1.cornerRadius = 20
         layer1.cornerCurve = .continuous
@@ -94,6 +89,21 @@ class AccountCardView: UIView {
         mask.contentMode = .scaleAspectFill
         mask.translatesAutoresizingMaskIntoConstraints = false
         return mask
+    }()
+    
+    private lazy var chainLargeImageContainer: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.spacing = 0
+        view.addArrangedSubview(chainLargeImageViewPadding)
+        view.addArrangedSubview(chainLargeImageView)
+        return view
+    }()
+    
+    private var chainLargeImageViewPadding: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
     }()
     
     private var nameLabel: UILabel = {
@@ -172,6 +182,8 @@ class AccountCardView: UIView {
     private static let chainButtonViewTag = 1
     private static let chainDotViewTag = 2
     
+    weak var shadowView: UIView?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
@@ -186,14 +198,16 @@ class AccountCardView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         backgroundLayer.frame = bounds
-        stackViewGradientLayer.frame = CGRect(x: bounds.origin.x,
-                                              y: chainsStackView.frame.origin.y - 13,
-                                              width: bounds.width,
-                                              height: 60)
-        stackViewTopBorderLayer.frame = CGRect(x: stackViewGradientLayer.frame.origin.x,
-                                               y: stackViewGradientLayer.frame.origin.y,
-                                               width: bounds.width,
-                                               height: 0.5)
+        stackViewBackgroudLayer.frame = CGRect(
+            x: bounds.origin.x,
+            y: chainsStackView.frame.origin.y - 13,
+            width: bounds.width,
+            height: 60)
+        stackViewTopBorderLayer.frame = CGRect(
+            x: stackViewBackgroudLayer.frame.origin.x,
+            y: stackViewBackgroudLayer.frame.origin.y,
+            width: bounds.width,
+            height: 0.5)
     }
     
     private func setup() {
@@ -202,7 +216,7 @@ class AccountCardView: UIView {
         directionalLayoutMargins = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16)
 
         layer.addSublayer(backgroundLayer)
-        addSubview(chainLargeImageView)
+        addSubview(chainLargeImageContainer)
         addSubview(maskImageView1)
         addSubview(maskImageView2)
         addSubview(maskImageView3)
@@ -214,17 +228,28 @@ class AccountCardView: UIView {
         addSubview(addressLabel)
         addSubview(copyButton)
         addSubview(balanceLabel)
+        layer.addSublayer(stackViewBackgroudLayer)
         addSubview(chainsStackView)
-        layer.addSublayer(stackViewGradientLayer)
         layer.addSublayer(stackViewTopBorderLayer)
         
+        chainLargeImageContainer.translatesAutoresizingMaskIntoConstraints = false
+        chainLargeImageViewPadding.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            chainLargeImageView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor,
-                                                     constant: 30),
-            chainLargeImageView.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor,
-                                                          constant: -20),
-            chainLargeImageView.widthAnchor.constraint(equalToConstant: 67),
-            chainLargeImageView.heightAnchor.constraint(equalToConstant: 67)
+            chainLargeImageContainer.leadingAnchor.constraint(
+                equalTo: maskImageView2.leadingAnchor),
+            chainLargeImageContainer.topAnchor.constraint(
+                equalTo: self.layoutMarginsGuide.topAnchor,
+                constant: 30
+            ),
+            // Set the position on the x-axis of `chainLargeImageView` according to the width of `maskImageView2`.
+            chainLargeImageViewPadding.widthAnchor.constraint(
+                equalTo: maskImageView2.widthAnchor,
+                multiplier: 53.0 / 158.0),
+            chainLargeImageView.widthAnchor.constraint(
+                equalTo: self.widthAnchor,
+                multiplier: 67.0 / 330),
+            chainLargeImageView.heightAnchor.constraint(
+                equalTo: chainLargeImageView.widthAnchor)
         ])
         
         NSLayoutConstraint.activate([
@@ -468,17 +493,26 @@ class AccountCardView: UIView {
         allChainButtons[selectChainIndex].viewWithTag(Self.chainDotViewTag)?.backgroundColor = .white
     }
     
-    private func updateBackground(isWalletConnect: Bool,
-                                  displayBlockchain: WalletDisplayBlockChainType) {
-        if isWalletConnect {
-            chainLargeImageView.image = Asset.Images.Scene.Balance.accountBgWc.image
-            backgroundLayer.colors = [
-                Asset.Colors.AccountCard.wcBackground1.color.cgColor,
-                Asset.Colors.AccountCard.wcBackground2.color.cgColor
-            ]
-        } else {
-            chainLargeImageView.image = displayBlockchain.chainBgImage
-            backgroundLayer.colors = displayBlockchain.accoundCardBgColors
+    private func updateBackground(
+        isWalletConnect: Bool,
+        displayBlockchain: WalletDisplayBlockChainType) {
+            let shadowLayer = shadowView?.layer
+            if isWalletConnect {
+                chainLargeImageView.image = Asset.Images.Scene.Balance.accountBgWc.image
+                backgroundLayer.colors = [
+                    Asset.Colors.AccountCard.wcBackground1.color.cgColor,
+                    Asset.Colors.AccountCard.wcBackground2.color.cgColor
+                ]
+                stackViewBackgroudLayer.backgroundColor =
+                    Asset.Colors.AccountCard.wcBackground3.color.cgColor
+                shadowLayer?.shadowColor =
+                    Asset.Colors.Shadow.Card.all.color.cgColor
+            } else {
+                chainLargeImageView.image = displayBlockchain.chainBgImage
+                backgroundLayer.colors = displayBlockchain.accoundCardBgColors
+                stackViewBackgroudLayer.backgroundColor = displayBlockchain.bottomBgColor
+                shadowLayer?.shadowColor = displayBlockchain.shadowColor
+            }
         }
-    }
 }
+// swiftlint:ensable force_cast line_length type_body_length file_length
