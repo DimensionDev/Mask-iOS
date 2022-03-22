69213c1db0e1bc4a9e09abfbd504795562bd41e0
Hugo <cythbgy@gmail.com>
2022-03-22 21:19:18 +0800

    feat(mm-304): contract of lucky drop (#67)
    * feat(mm-304): contract of lucky drop
    
    * fix: use enum instead of literal string
    
    Co-authored-by: Hugo L <hugol@MacBook-Pro-2.local>

---
+++ Maskbook/ABI/ABIContract.swift
@@ -0,0 +1,89 @@
+//
+//  ABIContract.swift
+//  Maskbook
+//
+//  Created by Hugo L on 2022/3/21.
+//  Copyright © 2022 dimension. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+import web3swift
+
+protocol ABIContract {
+    associatedtype Functions
+    associatedtype Events
+    
+    var mainCoordinator: Coordinator { get }
+    var contractAddress: EthereumAddress { get }
+    var abiString: String { get }
+    var abiVersion: Int { get }
+    var userSetting: UserDefaultSettings { get }
+}
+
+extension ABIContract {
+    var web3: web3 { Web3ProviderFactory.provider! }
+    var walletAddress: EthereumAddress { EthereumAddress(userSetting.defaultAccountAddress!)! }
+    
+    func read(
+        _ methodName: String,
+        param: [AnyObject]? = nil,
+        extraData: Data? = nil,
+        options: TransactionOptions? = nil
+    ) -> ReadTransaction? {
+        guard let contract = web3.contract(abiString, at: contractAddress, abiVersion: abiVersion) else {
+            return nil
+        }
+        var defaultOptions = TransactionOptions.defaultOptions
+        defaultOptions.from = walletAddress
+        defaultOptions.gasPrice = .automatic
+        defaultOptions.gasLimit = .automatic
+        guard let tx = contract.read(
+            methodName,
+            parameters: param ?? [],
+            extraData: extraData ?? Data(),
+            transactionOptions: options ?? defaultOptions) else {
+                return nil
+            }
+        return tx
+    }
+    
+    func write(
+        _ methodName: String,
+        param: [AnyObject]? = nil,
+        extraData: Data? = nil,
+        options: TransactionOptions? = nil
+    ) async -> String? {
+        guard let contract = web3.contract(abiString, at: contractAddress, abiVersion: abiVersion) else {
+            return nil
+        }
+        var defaultOptions = TransactionOptions.defaultOptions
+        defaultOptions.from = walletAddress
+        defaultOptions.gasPrice = .automatic
+        defaultOptions.gasLimit = .automatic
+        guard let tx = contract.write(
+            methodName,
+            parameters: param ?? [],
+            extraData: extraData ?? Data(),
+            transactionOptions: options ?? defaultOptions) else {
+                return nil
+            }
+        
+        let (promise, resolver) = Promise<String>.pending()
+        mainCoordinator.present(
+            scene: .maskSendResolverTransactionPopView(
+                resolver: resolver,
+                transaction: tx.transaction,
+                transactionOptions: tx.transactionOptions
+            ),
+            transition: .panModel(animated: true)
+        )
+        return await Task {
+            return try? promise.wait()
+        }.value
+    }
+}
+
+enum ABI {
+    static let happyRedPacketV4 = HappyRedPacketV4()
+}
