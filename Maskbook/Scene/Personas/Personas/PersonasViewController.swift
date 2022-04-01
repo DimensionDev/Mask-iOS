4d71cc027340a383ad5f9dcd48a4da9d2b4edfde
sunxiaojian <sunxiaojian@mask.io>
2022-04-01 12:25:28 +0800

    fix: adjust persona card animation

--- Maskbook/Scene/Personas/Personas/PersonasViewController.swift
+++ Maskbook/Scene/Personas/Personas/PersonasViewController.swift
@@ -72,7 +72,8 @@ class PersonasViewController: BaseViewController {
         let flowLayout = MnemonicVerifyCollectionFlowLayout()
         flowLayout.itemSize = PersonaCollectionDataSource.itemSize
         flowLayout.scrollDirection = .horizontal
-        flowLayout.sideItemScale = 0.9
+        flowLayout.sideItemXScale = 0.94
+        flowLayout.sideItemYScale = 0.85
         let view = ControlContainableCollectionView(frame: .zero, collectionViewLayout: flowLayout)
         view.backgroundColor = .clear
         view.showsHorizontalScrollIndicator = false
