6dbd9e2c2e34403de68fcaddf3242fa78d1498a1
sunxiaojian <sunxiaojian@mask.io>
2022-04-02 15:46:33 +0800

    feat: add camera access alert

--- Maskbook/Scene/Scanner/ScannerSource/ScannerPermission.swift
+++ Maskbook/Scene/Scanner/ScannerSource/ScannerPermission.swift
@@ -35,11 +35,24 @@ class ScannerPermission: NSObject {
             completion(false)
         }
     }
-    
+
     static func jumpToSystemPrivacySetting() {
         guard let appSetting = URL(string: UIApplication.openSettingsURLString) else {
             return
         }
         UIApplication.shared.open(appSetting, options: [:], completionHandler: nil)
+    }
+
+    static func showCameraAccessAlert(coordinator: Coordinator) {
+        let alertController = AlertController(title: L10n.Common.Alert.CameraAccess.title,
+                                              message: L10n.Common.Alert.CameraAccess.description,
+                                              confirmButtonText: L10n.Common.Controls.goSettings,
+                                              cancelButtonText: L10n.Common.Controls.dontAllow,
+                                              imageType: .noImage,
+                                              confirmButtonClicked: { _ in
+                                                  ScannerPermission.jumpToSystemPrivacySetting()
+                                              })
+        coordinator.present(scene: .alertController(alertController: alertController),
+                            transition: .alertController(completion: nil))
     }
 }
