cbe1a6f8f2e3df287c11d2113858ebb807e6d163
foxsin10 <yzjcnn@gmail.com>
2022-05-27 16:43:29 +0800

    refactor: refine hex prefix checking for rpid

--- Maskbook/Scene/App/RedPacket/ViewModel/LuckyDropHistoryViewModel+TokenHistory.swift
+++ Maskbook/Scene/App/RedPacket/ViewModel/LuckyDropHistoryViewModel+TokenHistory.swift
@@ -79,7 +79,7 @@ extension LuckyDropHistoryViewModel {
                     return nil
                 }
 
-                self.id = data.toHexString()
+                self.id = data.toHexString().addHexPrefix()
                 self.creation_time = time
             }
         }
@@ -122,7 +122,7 @@ extension LuckyDropHistoryViewModel {
                             return nil
                         }
 
-                        payload.basic?.rpid = "0x\(eventParam.id)"
+                        payload.basic?.rpid = eventParam.id
                         payload.basic?.creationTime = eventParam.creation_time.asDouble() ?? 0
 
                         try Task.checkCancellation()
