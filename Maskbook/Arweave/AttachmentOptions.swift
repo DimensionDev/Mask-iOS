28c2289b85a05dc1e07e314bc418014156366c7a
sxiaojian88 <875127234@qq.com>
2022-06-16 20:29:31 +0800

    fix: arweave big file upload

--- Maskbook/Arweave/AttachmentOptions.swift
+++ Maskbook/Arweave/AttachmentOptions.swift
@@ -26,7 +26,8 @@ struct AttachmentOptions {
 
     static let keyTable = "ABDEFGHJKMNPQRTWXYadefhijkmnprstuvwxyz03478"
 
-    static let magic_header = "MASKBOOK-ATTACHMENT"
+    
+    static let magic_header_data = "MASKBOOK-ATTACHMENT".data(using: .utf8)!
 
     static let defaultType = "application/octet-stream"
 }
@@ -75,7 +76,7 @@ extension AttachmentOptions {
             ] as [String: Any]
             var packData = Data()
             try packData.pack(payload)
-            return [Self.magic_header.data(using: .utf8)!,
+            return [Self.magic_header_data,
                     packData]
                 .combined
         } else {
@@ -91,18 +92,10 @@ extension AttachmentOptions {
             ] as [String: Any?]
             var packData = Data()
             try packData.pack(payload)
-            return [Self.magic_header.data(using: .utf8)!,
+            return [Self.magic_header_data,
                     packData]
                 .combined
         }
-    }
-
-    func toTransaction() throws -> ArweaveTransaction {
-        let encryptData = try encryptedData()
-        var transaction = ArweaveTransaction(data: encryptData)
-        let tag = ArweaveTransaction.Tag(name: "Content-Type", value: Self.defaultType)
-        transaction.tags.append(tag)
-        return transaction
     }
     
 }
