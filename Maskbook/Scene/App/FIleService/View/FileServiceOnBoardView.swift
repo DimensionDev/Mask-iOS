da04c836ac09d404a5842687e997571e6f168e44
foxsin10 <yzjcnn@gmail.com>
2022-06-27 14:29:41 +0800

    feat: refine recycling

--- Maskbook/Scene/App/FIleService/View/FileServiceOnBoardView.swift
+++ Maskbook/Scene/App/FIleService/View/FileServiceOnBoardView.swift
@@ -14,6 +14,9 @@ struct FileServiceOnBoardView: View {
     @StateObject
     private var viewModel = TimerViewModel()
 
+    @AppStorage("com.mask.io.fileService")
+    private var fileServicePolicyAccepted = false
+
     init(action: @escaping (Action) -> Void = { _ in }) {
         self.action = action
     }
@@ -23,36 +26,29 @@ struct FileServiceOnBoardView: View {
 
     var body: some View {
         GeometryReader { proxy in
-            ScrollViewReader { scrollProxy in
-                ScrollView(.horizontal) {
-                    LazyHStack(spacing: 0) {
-                        ForEach(viewModel.totalItems, id: \.self) { item in
-                            FileServiceOnBoardItemView(item: item.content)
-                                .id(item)
-                                .frame(width: proxy.size.width, height: proxy.size.height)
-                        }
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(viewModel.totalItems, id: \.self) { item in
+                        FileServiceOnBoardItemView(item: item.content)
+                            .id(item)
+                            .frame(width: proxy.size.width, height: proxy.size.height)
                     }
                 }
-                .introspectScrollView { scrollView in
-                    scrollView.isDirectionalLockEnabled = true
-                    scrollView.isPagingEnabled = true
-                    scrollView.delegate = viewModel
+            }
+            .introspectScrollView { scrollView in
+                scrollView.isDirectionalLockEnabled = true
+                scrollView.isPagingEnabled = true
+                scrollView.delegate = viewModel
+
+                if !viewModel.scrollViewDidScroll {
+                    viewModel.scrollViewDidScroll(scrollView)
+                    viewModel.scrollView = scrollView
+                    viewModel.scroll(toItem: viewModel.item)
                 }
-                .frame(width: proxy.size.width, height: proxy.size.height)
-                .onChange(of: viewModel.item) { newValue in
-                    if newValue.index > viewModel.maxIndex {
-                        let targetValue = viewModel.restartItem()
-                        scrollProxy.scrollTo(targetValue, anchor: .center)
-                        viewModel.item = targetValue
-                    } else {
-                        withAnimation(.easeInOut(duration: 1)) {
-                            scrollProxy.scrollTo(newValue, anchor: .center)
-                        }
-                    }
-                }
-                .onAppear {
-                    scrollProxy.scrollTo(viewModel.item)
-                }
+            }
+            .frame(width: proxy.size.width, height: proxy.size.height)
+            .onChange(of: viewModel.item) { newValue in
+                viewModel.scroll(toItem: newValue)
             }
             .overlay(
                 Image(Asset.Plugins.FileService.uploadBackground)
@@ -99,43 +95,53 @@ struct FileServiceOnBoardView: View {
         .colorScheme(.dark)
     }
 
+    private var policyAgreeView: some View {
+        HStack(alignment: .top) {
+            Group {
+                if fileServicePolicyAccepted {
+                    Color.white
+                        .frame(width: 16, height: 16)
+                        .cornerRadius(4)
+                        .overlay(
+                            Image(Asset.Plugins.checkMark)
+                        )
+                } else {
+                    Asset.Images.Scene.WalletList.unchecked.asImage()
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                        .frame(width: 16, height: 16)
+                }
+            }
+            .onTapGesture {
+                fileServicePolicyAccepted.toggle()
+            }
+
+            Spacer()
+        }
+    }
+
     private var policyView: some View {
         VStack(spacing: 20) {
-            HStack(alignment: .top) {
-                Group {
-                    if viewModel.checkBoxSelected {
-                        Color.white
-                            .frame(width: 16, height: 16)
-                            .cornerRadius(4)
-                            .overlay(
-                                Image(Asset.Plugins.checkMark)
-                            )
-                    } else {
-                        Asset.Images.Scene.WalletList.unchecked.asImage()
-                            .resizable()
-                            .renderingMode(.template)
-                            .foregroundColor(.white)
-                            .frame(width: 16, height: 16)
-                    }
-                }
-                .onTapGesture {
-                    viewModel.checkBoxSelected.toggle()
-                }
-
-                policyText
-                    .onTapGesture { action(.showPolicy) }
-
+            if fileServicePolicyAccepted {
                 Spacer()
+            } else {
+                policyAgreeView
             }
 
             Button(
-                action: { action(.upload) },
+                action: {
+                    if !fileServicePolicyAccepted {
+                        fileServicePolicyAccepted = true
+                    }
+                    action(.upload)
+                },
                 label: {
                     Color.white
                         .frame(height: 56)
                         .cornerRadius(8)
                         .opacity(
-                            viewModel.checkBoxSelected
+                            !fileServicePolicyAccepted
                             ? 1
                             : 0.5
                         )
@@ -146,7 +152,7 @@ struct FileServiceOnBoardView: View {
                         )
                 }
             )
-            .disabled(!viewModel.checkBoxSelected)
+            .disabled(!fileServicePolicyAccepted)
 
             Spacer()
         }
@@ -155,25 +161,22 @@ struct FileServiceOnBoardView: View {
     }
 
     private final class TimerViewModel: NSObject, ObservableObject, UIScrollViewDelegate {
-        @Published var item = Item.init(index: 0, content: .init(index: 0))
-        @Published var checkBoxSelected = false
-
+        @Published var item: Item
         @Published
         private(set) var totalItems: [Item] = []
-
         let maxIndex: Int
 
-        private var restartIndex: Int {
-            maxIndex / 2 - 1
-        }
+        private let dupilcateGroups = 3
+        private let groupItemCount = FileServiceOnBoardItem.allCases.count
 
-        private let dupilcateGroups = 25
-        private let groupItemCount: Int = FileServiceOnBoardItem.allCases.count
-
+        weak var scrollView: UIScrollView?
         private(set) var cancelableStorage: Set<AnyCancellable> = []
+
+        private(set) var scrollViewDidScroll = false
 
         struct Item: Hashable, Identifiable {
             let index: Int
+            let animated: Bool
             let content: FileServiceOnBoardItem
 
             var id: String {
@@ -181,29 +184,10 @@ struct FileServiceOnBoardView: View {
             }
         }
 
-         override init() {
-             maxIndex = dupilcateGroups * groupItemCount
-             super.init()
-             self.totalItems = (0 ..< maxIndex).map {
-                 Item(index: $0, content: FileServiceOnBoardItem(index: $0))
-             }
-             item = restartItem()
-        }
-
-        func startTimer() {
-            cancelableStorage = []
-            Timer.publish(every: 2, on: .main, in: .common)
-                .autoconnect()
-                .receive(on: RunLoop.main)
-                .sink { [weak self] _ in
-                    guard let self = self else { return }
-                    self.item = .init(index: self.item.index + 1, content: self.item.content.next)
-                }
-                .store(in: &cancelableStorage)
-        }
-
-        func restartItem() -> Item {
-            func restartIndex() -> Int {
+        override init() {
+            let groupItemCount = FileServiceOnBoardItem.allCases.count
+            let maxIndex = dupilcateGroups * groupItemCount
+            let startIndex: Int = {
                 var middle = maxIndex / 2
                 while middle > 0 {
                     switch middle % groupItemCount {
@@ -213,33 +197,125 @@ struct FileServiceOnBoardView: View {
                 }
 
                 return 0
-            }
+            }()
+            let content = FileServiceOnBoardItem(index: startIndex)
+            item = .init(index: startIndex, animated: false, content: content)
+            self.maxIndex = maxIndex
 
+            super.init()
+            self.totalItems = (0 ..< maxIndex).map {
+                Item(index: $0, animated: true, content: FileServiceOnBoardItem(index: $0))
+            }
+        }
+
+        func startTimer() {
+            cancelableStorage = []
+            Timer.publish(every: 2, on: .main, in: .common)
+                .autoconnect()
+                .receive(on: RunLoop.main)
+                .sink { [weak self] _ in
+                    guard let self = self else { return }
+                    let targetIndex =  self.item.index + 1
+                    let targetValue = Item.init(index: targetIndex, animated: true, content: .init(index: targetIndex))
+                    self.item = targetValue
+
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                        self.timerCheckIfNeedToUpdateTargetIndex(targetIndex)
+                    }
+                }
+                .store(in: &cancelableStorage)
+        }
+
+        func restartItem() -> Item {
             let index = restartIndex()
             let content = FileServiceOnBoardItem(index: index)
-            return .init(index: index, content: content)
+            return .init(index: index, animated: false, content: content)
+        }
+
+        private func timerCheckIfNeedToUpdateTargetIndex(_ targetIndex: Int) {
+            switch targetIndex {
+            case (maxIndex - 1)... :
+                let restartIndex = restartIndex() + groupItemCount - 1
+                let targetValue = Item.init(index: restartIndex, animated: false, content: .init(index: restartIndex))
+                item = targetValue
+            // normaly this case will not happen and is being handled in `func scrollViewDidEndDecelerating(_:)`
+            case ...0: break
+            default: break
+            }
+        }
+
+        private func restartIndex() -> Int {
+            var middle = maxIndex / 2
+            while middle > 0 {
+                switch middle % groupItemCount {
+                case 0: return middle
+                default: middle -= 1
+                }
+            }
+
+            return 0
+        }
+
+        func updateTargetItem(currentIndex: Int, animateFlag: Bool? = nil) {
+            switch currentIndex {
+            case (maxIndex - 1)... :
+                let restartIndex = restartIndex() + groupItemCount - 1
+                let targetValue = Item.init(index: restartIndex, animated: false, content: .init(index: restartIndex))
+                item = targetValue
+
+            case ...0:
+                let restartIndex = restartIndex()
+                let targetValue = Item.init(index: restartIndex, animated: false, content: .init(index: restartIndex))
+                item = targetValue
+
+            default:
+                let targetValue = Item(index: currentIndex, animated: true, content: .init(index: currentIndex))
+                item = targetValue
+            }
+        }
+
+        func scroll(toItem item: Item) {
+            func contentOffset(for item: Item) -> CGPoint {
+                guard let scrollView = self.scrollView,
+                      scrollView.frame != .zero,
+                      scrollView.contentSize != .zero else {
+                    return .zero
+                }
+
+                return CGPoint(x: scrollView.frame.width * CGFloat(item.index), y: 0)
+            }
+
+            let offset = contentOffset(for: item)
+            scrollView?.setContentOffset(offset, animated: item.animated)
         }
 
         private func stopTimer() {
             cancelableStorage = []
         }
 
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            scrollViewDidScroll = true
+        }
+
         func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+            self.scrollView <| scrollView
             stopTimer()
         }
 
         func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+            self.scrollView <| scrollView
             startTimer()
         }
 
         func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+            self.scrollView <| scrollView
             guard scrollView.frame.width > 0 else {
                 return
             }
             let frameWidth = scrollView.frame.width
             let index = Int(scrollView.contentOffset.x / frameWidth)
-            let targetValue = Item(index: index, content: .init(index: index))
-            item <| targetValue
+
+            updateTargetItem(currentIndex: index)
         }
     }
 }
