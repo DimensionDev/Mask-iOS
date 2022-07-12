e70996ba92c96b9a89231dde5faffe99648ad36f
sxiaojian88 <875127234@qq.com>
2022-07-12 14:53:56 +0800

    fix: add final

--- Maskbook/Scene/App/RedPacket/ViewModel/PluginMetaShareViewModel.swift
+++ Maskbook/Scene/App/RedPacket/ViewModel/PluginMetaShareViewModel.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class PluginMetaShareViewModel: NSObject {
+final class PluginMetaShareViewModel: NSObject {
     @InjectedProvider(\.mainCoordinator)
     private var coordinator
 
