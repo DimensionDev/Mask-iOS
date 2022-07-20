16352ce0e27552d5a0cd3979b27ff3f9e5cae4b3
foxsin10 <yzjcnn@gmail.com>
2022-07-20 12:23:26 +0800

    fix: fix leaks in `LuckyDropViewModel` and `WalletBottomWidgetViewModel`

--- Maskbook/Scene/Wallet/WalletHistory/WalletHistoryViewController.swift
+++ Maskbook/Scene/Wallet/WalletHistory/WalletHistoryViewController.swift
@@ -97,7 +97,6 @@ class WalletHistoryViewController: BaseViewController {
             .receive(on: DispatchQueue.main)
             .sink { _ in }
         receiveValue: { [weak self] allTransactions, allPendingTransactions in
-            let allPendingTransactions = allPendingTransactions.compactMap { $0 as? PendingTransaction }
             let pendingTransactions = allPendingTransactions.filter {
                 $0.address == maskUserDefaults.defaultAccountAddress &&
                 $0.networkId == maskUserDefaults.network.networkId
