c37168fc24e769c7bddd5c5e9e83674ae2a04489
hydraCy <461189296@qq.com>
2022-08-03 11:07:49 +0800

    style: code format

--- Maskbook/Scene/Wallet/Balance/View/BalanceAccountCell.swift
+++ Maskbook/Scene/Wallet/Balance/View/BalanceAccountCell.swift
@@ -17,15 +17,15 @@ protocol BalanceAccountCellDelegate: AnyObject {
 
 class BalanceAccountCell: UITableViewCell {
     var accountCardView: AccountCardView
-    
+
     private var disposeBag = Set<AnyCancellable>()
-    
+
     private var accountCardShadowView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     private var actionsStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
@@ -35,120 +35,134 @@ class BalanceAccountCell: UITableViewCell {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-    
+
     private var sendView: ActionView = {
         let view = ActionView()
-        view.configWith(image: Asset.Images.Scene.Balance.Button.send.image, title: L10n.Scene.WalletBalance.btnSend)
-        view.addSelfConstraints()
+        view.config(image: Asset.Images.Scene.Balance.Button.send.image, title: L10n.Scene.WalletBalance.btnSend)
+        view.addLayoutConstraints()
         return view
     }()
-    
+
     private var receiveView: ActionView = {
         let view = ActionView()
-        view.configWith(image: Asset.Images.Scene.Balance.Button.receive.image, title: L10n.Scene.WalletBalance.btnReceive)
-        view.addSelfConstraints()
+        view.config(image: Asset.Images.Scene.Balance.Button.receive.image, title: L10n.Scene.WalletBalance.btnReceive)
+        view.addLayoutConstraints()
         return view
     }()
-    
+
     private var historyView: ActionView = {
         let view = ActionView()
-        view.configWith(image: Asset.Images.Scene.Balance.Button.history.image, title: L10n.Scene.WalletDetail.walletItemsHistory)
-        view.addSelfConstraints()
+        view.config(image: Asset.Images.Scene.Balance.Button.history.image, title: L10n.Scene.WalletDetail.walletItemsHistory)
+        view.addLayoutConstraints()
         return view
     }()
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         let shadowLayer = accountCardShadowView.layer
         shadowLayer.shadowOpacity = 1
         shadowLayer.shadowOffset = .zero
         shadowLayer.shadowRadius = 10
-        
-        let rect = CGRect(x: 36,
-                          y: accountCardView.bounds.height,
-                          width: accountCardView.bounds.width - 72,
-                          height: 10)
+
+        let rect = CGRect(
+            x: 36,
+            y: accountCardView.bounds.height,
+            width: accountCardView.bounds.width - 72,
+            height: 10
+        )
         shadowLayer.shadowPath = UIBezierPath(roundedRect: rect, cornerRadius: 20).cgPath
     }
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         accountCardView = AccountCardView()
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         _init()
     }
-    
+
     required init?(coder: NSCoder) {
         accountCardView = AccountCardView()
         super.init(coder: coder)
         _init()
     }
-    
+
     weak var delegate: BalanceAccountCellDelegate?
     private func _init() {
         accountCardView.shadowView = accountCardShadowView
-        
+
         selectionStyle = .none
         backgroundColor = .clear
-        
+
         accountCardView.delegate = self
         accountCardView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         contentView.addSubview(accountCardShadowView)
         contentView.addSubview(accountCardView)
         contentView.addSubview(actionsStackView)
-        
+
         NSLayoutConstraint.activate([
             accountCardView.topAnchor.constraint(
                 equalTo: contentView.safeAreaLayoutGuide.topAnchor,
-                constant: LayoutConstraints.top),
+                constant: LayoutConstraints.top
+            ),
             accountCardView.leadingAnchor.constraint(equalTo: contentView.readableContentGuide.leadingAnchor),
             accountCardView.trailingAnchor.constraint(equalTo: contentView.readableContentGuide.trailingAnchor),
-            accountCardView.heightAnchor.constraint(equalToConstant: 186)
+            accountCardView.heightAnchor.constraint(equalToConstant: 186),
         ])
-        
+
         NSLayoutConstraint.activate([
             accountCardShadowView.leadingAnchor.constraint(equalTo: accountCardView.leadingAnchor),
             accountCardShadowView.topAnchor.constraint(equalTo: accountCardView.topAnchor),
             accountCardShadowView.trailingAnchor.constraint(equalTo: accountCardView.trailingAnchor),
-            accountCardShadowView.bottomAnchor.constraint(equalTo: accountCardView.bottomAnchor)
+            accountCardShadowView.bottomAnchor.constraint(equalTo: accountCardView.bottomAnchor),
         ])
-        
+
         actionsStackView.addArrangedSubview(sendView)
         actionsStackView.addArrangedSubview(receiveView)
         actionsStackView.addArrangedSubview(historyView)
-        
+
         NSLayoutConstraint.activate([
-            actionsStackView.topAnchor.constraint(equalTo: accountCardView.bottomAnchor,
-                                                  constant: 22),
-            actionsStackView.leadingAnchor.constraint(equalTo: accountCardView.leadingAnchor,
-                                                      constant: 12),
-            actionsStackView.trailingAnchor.constraint(equalTo: accountCardView.trailingAnchor,
-                                                       constant: -12),
+            actionsStackView.topAnchor.constraint(
+                equalTo: accountCardView.bottomAnchor,
+                constant: 22
+            ),
+            actionsStackView.leadingAnchor.constraint(
+                equalTo: accountCardView.leadingAnchor,
+                constant: 12
+            ),
+            actionsStackView.trailingAnchor.constraint(
+                equalTo: accountCardView.trailingAnchor,
+                constant: -12
+            ),
             actionsStackView.heightAnchor.constraint(equalToConstant: 61),
-            actionsStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,
-                                                     constant: -16)
+            actionsStackView.bottomAnchor.constraint(
+                equalTo: contentView.bottomAnchor,
+                constant: -16
+            ),
         ])
-        
+
         sendView
             .cv.tapGesture()
             .sink { [weak self] _ in
-                guard let self = self else { return }
+                guard let self = self else {
+                    return
+                }
                 let mainToken = WalletAssetManager.shared.getDefaultMainToken()
-                
+
                 if let quantity = mainToken?.quantity {
                     if quantity.compare(NSDecimalNumber.zero) == .orderedSame {
                         self.showBalanceNotEnoughAlert()
                     } else {
                         Coordinator.main.present(
                             scene: .sendTransaction(param: nil),
-                            transition: .detail(animated: true))
+                            transition: .detail(animated: true)
+                        )
                     }
                 } else {
                     self.showBalanceNotEnoughAlert()
                 }
             }
             .store(in: &disposeBag)
-        
+
         receiveView
             .cv.tapGesture()
             .sink { _ in
@@ -159,11 +173,13 @@ class BalanceAccountCell: UITableViewCell {
                     scene: .receiveAddress(
                         network: maskUserDefaults.network,
                         token: nil,
-                        address: defaultAddress),
-                    transition: .detail(animated: true))
+                        address: defaultAddress
+                    ),
+                    transition: .detail(animated: true)
+                )
             }
             .store(in: &disposeBag)
-        
+
         historyView
             .cv.tapGesture()
             .sink { _ in
@@ -171,14 +187,14 @@ class BalanceAccountCell: UITableViewCell {
             }
             .store(in: &disposeBag)
     }
-    
+
     func setup(account: Account?, portfolio: Portfolio?) {
         accountCardView.setup(account: account, portfolio: portfolio)
     }
 }
 
 extension BalanceAccountCell: AccountCardViewDelegate {
-    func moreButtonDidClick(view: AccountCardView, button: UIButton) {
+    func moreButtonDidClick(view _: AccountCardView, button: UIButton) {
         delegate?.balanceAccountCell(cell: self, button: button)
     }
 }
@@ -186,11 +202,13 @@ extension BalanceAccountCell: AccountCardViewDelegate {
 extension BalanceAccountCell {
     func showBalanceNotEnoughAlert() {
         let mainToken = maskUserDefaults.network.mainToken
-        guard let symbol = mainToken?.symbol.uppercased() else { return }
-        
+        guard let symbol = mainToken?.symbol.uppercased() else {
+            return
+        }
+
         let des = L10n.Common.Alert.TokenBalance.description(symbol, symbol)
         let btnConfirm = L10n.Common.Alert.TokenBalance.btnConfirm(symbol)
-        
+
         let alertController = AlertController(
             title: "",
             message: des,
@@ -200,10 +218,13 @@ extension BalanceAccountCell {
             confirmButtonClicked: { _ in
                 Coordinator.main.present(scene: .showTransakIntegration, transition: .modal())
             },
-            cancelButtonClicked: nil)
-        Coordinator.main.present(scene: .alertController(alertController:
-            alertController),
-        transition: .alertController(completion: nil))
+            cancelButtonClicked: nil
+        )
+        Coordinator.main.present(
+            scene: .alertController(alertController:
+                alertController),
+            transition: .alertController(completion: nil)
+        )
     }
 }
 
@@ -213,14 +234,14 @@ extension BalanceAccountCell {
             let imageView = UIImageView()
             return imageView
         }()
-    
+
         let titleLabel: UILabel = {
             let label = UILabel()
             label.font = FontStyles.BH6
             label.textColor = Asset.Colors.Text.dark.color
             return label
         }()
-    
+
         init() {
             super.init(frame: .zero)
             withSubViews {
@@ -231,28 +252,28 @@ extension BalanceAccountCell {
                 imageView.widthAnchor.constraint(equalToConstant: 36),
                 imageView.heightAnchor.constraint(equalToConstant: 36),
                 imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-                imageView.topAnchor.constraint(equalTo: topAnchor)
+                imageView.topAnchor.constraint(equalTo: topAnchor),
             ])
             NSLayoutConstraint.activate([
                 titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4),
-                titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+                titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             ])
         }
 
         @available(*, unavailable)
-        required init?(coder: NSCoder) {
+        required init?(coder _: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
-    
-        func addSelfConstraints() {
+
+        func addLayoutConstraints() {
             let width = (UIScreen.main.bounds.width - LayoutConstraints.leading * 2) / 3
             NSLayoutConstraint.activate([
                 heightAnchor.constraint(equalToConstant: 61),
-                widthAnchor.constraint(equalToConstant: width).priority(.required - 1)
+                widthAnchor.constraint(equalToConstant: width).priority(.required - 1),
             ])
         }
-    
-        func configWith(image: UIImage, title: String) {
+
+        func config(image: UIImage, title: String) {
             imageView.image = image
             titleLabel.text = title
         }
