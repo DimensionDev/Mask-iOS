95a018489af197f0ea1b7de60c67e2659345096b
sxiaojian88 <875127234@qq.com>
2022-08-11 18:25:35 +0800

    fix: NFTLuckyDropClaimDetail scene display image

---
+++ Maskbook/Provider/Repository/CollectibleRepository.swift
@@ -0,0 +1,30 @@
+//
+//  CollectibleRepository.swift
+//  Maskbook
+//
+//  Created by xiaojian sun on 2022/8/11.
+//  Copyright © 2022 dimension. All rights reserved.
+//
+
+import CoreData
+import CoreDataStack
+import Foundation
+
+final class CollectibleRepository {
+    static let viewContext = AppContext.shared.coreDataStack.persistentContainer.viewContext
+
+    static func queryCollection(address: String,
+                                network: BlockChainNetwork,
+                                tokenId: String) -> Collectible?
+    {
+        do {
+            let queryContext = viewContext
+            let fetchRequest = Collectible.sortedFetchRequest
+            fetchRequest.predicate = Collectible.predicate(address: address, networkId: network.networkId, tokenId: tokenId)
+            fetchRequest.fetchLimit = 1
+            return try queryContext.fetch(fetchRequest).first
+        } catch {
+            return nil
+        }
+    }
+}
